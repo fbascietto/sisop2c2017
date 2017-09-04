5e15d3c606e412c7cc9b23337b4225d1ff239cd6
Francisco Bascietto <fran.ba22@gmail.com>
2017-09-04 00:05:24 -0300

    Se agrega la función help.

--- FileSystem/funcionesfs.c
+++ FileSystem/funcionesfs.c
@@ -62,10 +62,30 @@ escucharConsola(){
 			printf("Seleccionaste obtener informacion\n");
 
 		}
+		if(!strncmp(linea, "help",4)) {
+			  printf("YamaFS Ayuda\n");
+			  printf("Los parámetros se indican con [] \n------\n");
+			  printf("format - Formatear el Filesystem.\n");
+			  printf("rm [path_archivo] ó rm -d [path_directorio] ó rm -b [path_archivo] [nro_bloque] [nro_copia] - Eliminar un Archivo/Directorio/Bloque.\n");
+			  printf("mv [path_original] [path_final] - Mueve un Archivo o Directorio.\n");
+			  printf("cat [path_archivo] - Muestra el contenido del archivo como texto plano.\n");
+			  printf("mkdir [path_dir] - Crea un directorio. Si el directorio ya existe, el comando deberá informarlo.\n");
+			  printf("cpfrom [path_archivo_origen] [directorio_yamafs] - Copiar un archivo local al yamafs, siguiendo los lineamientos en la operación Almacenar Archivo de la Interfaz del FileSystem.\n");
+			  printf("cpto [path_archivo_yamafs] [directorio_filesystem] - Copiar un archivo local al yamafs.\n");
+			  printf("cpblock [path_archivo] [nro_bloque] [id_nodo] - Crea una copia de un bloque de un archivo en el nodo dado.\n");
+			  printf("md5 [path_archivo_yamafs] - Solicitar el MD5 de un archivo en yamafs.\n");
+			  printf("ls [path_directorio] - Lista los archivos de un directorio.\n");
+			  printf("info [path_archivo] - Muestra toda la información del archivo, incluyendo tamaño, bloques, ubicación de los bloques, etc.\n");
+			  printf("exit - Sale del programa.\n");
+		}
 		else {
-			printf("no se reconoce el comando %s\n",linea);
+			printf("No se reconoce el comando %s\n",linea);
+			printf("Para más información utilice el comando ""help"".\n");
 		}
 
 		free(linea);
   }
 }
+
+
+
