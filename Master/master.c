a7093fd9ed5d76a3eb94618d4ddff6915469353d
Francisco Bascietto <fran.ba22@gmail.com>
2017-09-13 01:14:10 -0300

    Se agrega uso de config.h en master

--- Master/master.c
+++ Master/master.c
@@ -6,10 +6,27 @@
  */
 
 #include "funcionesmaster.h"
+#include <stdio.h>
+#include <string.h>
+#include <commons/config.h>
 
 void main() {
 	//leer config
+
+	t_config* infoConfig;
+	char* yamaIP;
+	int yamaPort;
+
+	infoConfig = config_create("/home/utnso/yama/tp-2017-2c-sapnu-puas/Master/config.txt");
+
+	if(config_has_property(infoConfig,"YAMA_IP")){
+		yamaIP = config_get_string_value(infoConfig,"YAMA_IP");
+		printf("IP: %s\n", yamaIP);
+	}
+
+	if(config_has_property(infoConfig,"YAMA_PUERTO")){
+			yamaPort = config_get_int_value(infoConfig,"YAMA_PUERTO");
+			printf("Puerto: %d\n", yamaPort);
+	}
 }
-
-
 
