07f766aa7fcf098585c8a59e2abb6039d8053dad
alabarque <agustinlabarc@hotmail.com>
2017-10-07 01:43:40 -0300

    agrego algoritmo clock

--- Yama/funcionesyama.h
+++ Yama/funcionesyama.h
@@ -6,6 +6,7 @@
  */
 
 #include <sys/socket.h>
+#include <commons/collections/list.h>
 
 #ifndef YAMA_FUNCIONESYAMA_H_
 #define YAMA_FUNCIONESYAMA_H_
@@ -15,6 +16,14 @@ typedef struct {
 	fd_set fdSocketEscucha;
 } t_esperar_conexion;
 
+typedef struct{
+	int disponibilidad;
+	t_list * bloques;
+}t_nodo;
+
 void *esperarConexionesYama(void *args);
+void prePlanificacionClock(int bloques, int dispBase, t_list* listaWorkers);
+int siguienteWorker(int numNodo, t_list* nodos, t_nodo* nodo);
+void agregarWorkerALaPlanificacion(int bloque, t_nodo* worker);
 
 #endif /* YAMA_FUNCIONESYAMA_H_ */
