9055263137ad56c8b606b7740f1fe7de5b683d6b
GONZALO LEPETIC <gonzalolepetic@hotmail.com>
2017-10-16 15:22:51 -0300

    Mock de estrucutura para trabajar localmente

--- Nodo/Worker/interfaceWorker.c
+++ Nodo/Worker/interfaceWorker.c
@@ -9,12 +9,21 @@
 
 solicitud_programa_transformacion* deserializarSolicitudProgramaTransformacion(char* message){
 
-	solicitud_programa_transformacion* mock;
+	solicitud_programa_transformacion* mock = malloc(sizeof(solicitud_programa_transformacion));
+	strcpy(mock->programa_transformacion, "/scripts/transformador.sh");
+	mock->bloque = 1;
+	mock->bytes_ocupados = 0;
+	strcpy(mock->archivo_temporal, "/tmp/Master1-temp1");
 	return mock;
 }
 
 solicitud_programa_reduccion_local* deserializarSolicitudProgramaReduccionLocal(char* message){
 
-	solicitud_programa_reduccion_local* mock;
+	archivo_temp* lista_de_temps = malloc(sizeof(archivo_temp));
+	solicitud_programa_reduccion_local* mock = malloc(sizeof(solicitud_programa_reduccion_local));
+	strcpy(mock->programa_reduccion, "/scripts/reductor.rb");
+	mock->archivos_temporales = lista_de_temps;
+	mock->cantidad_archivos_temp = 5;
+	strcpy(mock->archivo_temporal_resultante, "/tmp/Master1-Worker1");
 	return mock;
 }
